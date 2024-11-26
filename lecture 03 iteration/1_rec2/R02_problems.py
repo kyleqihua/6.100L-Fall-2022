14b67d544009b5fce0320943815a64d818473d4d
kyleqihua <kyleqihuahuang@gmail.com>
2024-11-26 08:45:42 +0800

    lec 3 rec done

--- lecture 03 iteration/1_rec2/R02_problems.py
+++ lecture 03 iteration/1_rec2/R02_problems.py
@@ -3,7 +3,8 @@
 # Write a program that takes your name as an Input and Outputs the length of your name minus 5.
 
 # Insert code below
-
+# name = input("please enter your name ")
+# print(len(name) - 5)
 
 ####################################################################################
 # Practice Problem 2
@@ -14,7 +15,9 @@ test_string = "We want to remove the nth character from this string"
 n = 8
 
 # Insert code below
-
+# print("this is the old string:", test_string)
+# new_string = test_string[0:n-1] + test_string[n:len(test_string)]
+# print("this is the new string:", new_string)
 
 
 ####################################################################################
@@ -25,7 +28,10 @@ n = 8
 my_string = "This is my string"  # example string - modify to test
 
 # Insert code below
-
+# if len(my_string) > 10 or len(my_string) < 5:
+#     print(True)
+# else:
+#     print(False) 
 
 
 
@@ -37,7 +43,11 @@ my_string = "This is my string"  # example string - modify to test
 my_string = "How many times is the letter e in this string?"
 
 # Insert code below
-
+sum = 0
+for i in my_string:
+    if i == "e":
+        sum = sum + 1
+print(sum)
 
 
 
